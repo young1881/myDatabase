616f761917d8c930e92e78b89c0fb0d3988598be
Steven Young <ywch2002@126.com>
2022-07-12 10:50:20 +0800

    statement struct

--- main.cpp
+++ main.cpp
@@ -3,10 +3,26 @@
 
 using namespace std;
 
+enum MetaCommandResult {
+    META_COMMAND_SUCCESS, META_COMMAND_UNRECOGNIZED
+};
+
+enum PrepareResult {
+    PREPARE_SUCCESS, PREPARE_UNRECOGNIZED_STATEMENT
+};
+
+enum StatementType {
+    INSERT, SELECT
+};
+
 struct InputBuffer {
     string buffer;
     size_t bufferLength{};
     ssize_t inputLength{};
+};
+
+struct Statement {
+    StatementType type;
 };
 
 InputBuffer *new_input_buffer();
@@ -14,6 +30,12 @@ InputBuffer *new_input_buffer();
 void print_prompt();
 
 void read_input(InputBuffer *inputBuffer);
+
+MetaCommandResult do_meta_command(InputBuffer *inputBuffer);
+
+PrepareResult prepare_statement(InputBuffer *inputBuffer, Statement *statement);
+
+void execute_statement(Statement *statement);
 
 void close_input_buffer(InputBuffer *inputBuffer);
 
@@ -23,12 +45,30 @@ int main(int argc, char *argv[]) {
         print_prompt();
         read_input(inputBuffer);
 
-        if (inputBuffer->buffer == ".exit") {
-            close_input_buffer(inputBuffer);
-            exit(EXIT_SUCCESS);
-        } else {
-            cout << "Unrecognized command " << inputBuffer->buffer << "\n";
+        // Non-SQL statements like .exit are called “meta-commands”.
+        if (inputBuffer->buffer[0] == '.') {
+            switch (do_meta_command(inputBuffer)) {
+                case (META_COMMAND_SUCCESS):
+                    continue;
+                case (META_COMMAND_UNRECOGNIZED):
+                    cout << "Unrecognized command" << inputBuffer->buffer
+                         << ".\n";
+                    continue;
+            }
         }
+
+        Statement statement{};
+        switch (prepare_statement(inputBuffer, &statement)) {
+            case (PREPARE_SUCCESS):
+                break;
+            case (PREPARE_UNRECOGNIZED_STATEMENT):
+                cout << "Unrecongnized keyword at the start of "
+                     << inputBuffer->buffer << ".\n";
+                continue;
+        }
+
+        execute_statement(&statement);
+        cout << "Executed.\n";
     }
 }
 
@@ -50,6 +90,39 @@ void read_input(InputBuffer *inputBuffer) {
     if (inputBuffer->buffer.empty()) {
         cout << "Error reading input\n";
         exit(EXIT_FAILURE);
+    }
+}
+
+MetaCommandResult do_meta_command(InputBuffer *inputBuffer) {
+    if (inputBuffer->buffer == ".exit") {
+        exit(EXIT_SUCCESS);
+    } else {
+        return META_COMMAND_UNRECOGNIZED;
+    }
+}
+
+PrepareResult prepare_statement(InputBuffer *inputBuffer,
+                                Statement *statement) {
+    if (inputBuffer->buffer.substr(0, 6) == "insert") {
+        statement->type = INSERT;
+        return PREPARE_SUCCESS;
+    }
+    if (inputBuffer->buffer == "select") {
+        statement->type = SELECT;
+        return PREPARE_SUCCESS;
+    }
+
+    return PREPARE_UNRECOGNIZED_STATEMENT;
+}
+
+void execute_statement(Statement *statement) {
+    switch (statement->type) {
+        case (INSERT):
+            cout << "This is where we would do an insert.\n";
+            break;
+        case (SELECT):
+            cout << "This is where we would do a select.\n";
+            break;
     }
 }
 
